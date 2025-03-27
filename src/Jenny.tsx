e89fbfd0ec5a78afc7bd811815d72eeea54e112a
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-03-27 10:56:57 +0100

    add jenny intro section

--- src/Jenny.tsx
+++ src/Jenny.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import "./Jenny.css";
+import lotus from "./assets/crossed-legs.jpg";
 
 function Jenny() {
   return (
     <>
       <div className="Jenny">
-        <h1>Jenny</h1>
+        <div className="jenny-image">
+          <img className="lotus" src={lotus} alt="lotus position" />
+        </div>
+        <div className="jenny-text">
+          <h1>Je suis Jenny</h1>
+          <p className="jenny-intro-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            rerum illum consectetur. Fugiat consequuntur debitis tempora ut,
+            cumque eaque deleniti dolores minus quis mollitia soluta. Quidem
+            nemo in minus blanditiis?
+          </p>
+        </div>
       </div>
     </>
   );
