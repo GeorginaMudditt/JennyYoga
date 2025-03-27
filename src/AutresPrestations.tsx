e36f0320eafce1020bfb0d721d71f4f25cdd9a00
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-03-27 20:48:21 +0100

    add descriptions and buttons to AutrePrestations section

--- src/AutresPrestations.tsx
+++ src/AutresPrestations.tsx
@@ -13,32 +13,41 @@ function AutresPrestations() {
           <h1>Autres Prestations</h1>
           <h2 id="meditation-section">Méditation</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
-            saepe alias, architecto nisi dignissimos officiis nihil officia
-            quibusdam harum iure.
+            La méditation est une pratique de concentration qui apaise l’esprit
+            et favorise la clarté mentale. Par la respiration, la visualisation
+            ou la pleine conscience, elle réduit le stress, améliore la
+            concentration et cultive la paix intérieure. Une pratique régulière
+            équilibre les émotions et renforce le bien-être.
           </p>
+          <button className="btn">Plus d'informations</button>
           <h2 id="massage-section">Massage et soins énergétiques</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
-            saepe alias, architecto nisi dignissimos officiis nihil officia
-            quibusdam harum iure.
+            Le massage et les soins énergétiques allient le toucher et des
+            techniques holistiques pour rétablir l’équilibre physique et
+            énergétique. Par la pression, le mouvement ou le travail
+            énergétique, ils soulagent les tensions, améliorent la circulation
+            et harmonisent le flux naturel du corps, favorisant détente et
+            bien-être.
           </p>
+          <button className="btn">Plus d'informations</button>
           <h2 id="coaching-section">Coaching Holistique</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
-            saepe alias, architecto nisi dignissimos officiis nihil officia
-            quibusdam harum iure.
+            Le coaching holistique accompagne le développement personnel en
+            intégrant l’esprit, le corps et les émotions. Par l’accompagnement
+            et l’introspection, il aide à trouver l’équilibre, surmonter les
+            défis et atteindre ses objectifs, favorisant bien-être et
+            épanouissement.
           </p>
+          <button className="btn">Plus d'informations</button>
           <h2 id="sauna-section">Sauna | Hammam</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
-            saepe alias, architecto nisi dignissimos officiis nihil officia
-            quibusdam harum iure.
+            Le sauna et le hammam sont des thérapies de relaxation par la
+            chaleur qui détoxifient le corps et procurent une détente profonde.
+            Le sauna diffuse une chaleur sèche, tandis que le hammam offre de la
+            vapeur. Ils améliorent la circulation, soulagent les tensions
+            musculaires et purifient la peau, favorisant le bien-être.
           </p>
+          <button className="btn">Plus d'informations</button>
         </div>
       </div>
     </>
