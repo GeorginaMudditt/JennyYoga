24638be166a1f159bb86ceaea15860ff17fbba48
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-03-27 12:22:08 +0100

    add content to yoga and autresprestations

--- src/AutresPrestations.tsx
+++ src/AutresPrestations.tsx
@@ -1,11 +1,45 @@
 import "./App.css";
 import "./AutresPrestations.css";
+import massage from "./assets/massage.jpg";
 
 function AutresPrestations() {
   return (
     <>
       <div className="AutresPrestations">
-        <h1>Autres Prestations</h1>
+        <div className="massage-image">
+          <img className="massage" src={massage} alt="back massage" />
+        </div>
+        <div className="massage-text">
+          <h1>Autres Prestations</h1>
+          <h2>Méditation</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Massage et soins énergétiques</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Coaching Holistique</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Sauna | Hammam</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+        </div>
       </div>
     </>
   );
--- src/Yoga.tsx
+++ src/Yoga.tsx
@@ -1,11 +1,45 @@
 import "./App.css";
 import "./Yoga.css";
+import outside from "./assets/outside.jpg";
 
 function Yoga() {
   return (
     <>
       <div className="Yoga">
-        <h1>Yoga</h1>
+        <div className="yoga-text">
+          <h1>Yoga</h1>
+          <h2>Vinyasa</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Postural</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Yin yoga</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+          <h2>Yoga danse</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            eaque. Similique illo expedita quae facere aut, incidunt esse itaque
+            saepe alias, architecto nisi dignissimos officiis nihil officia
+            quibusdam harum iure.
+          </p>
+        </div>
+        <div className="yoga-image">
+          <img className="outside-yoga" src={outside} alt="outside yoga" />
+        </div>
       </div>
     </>
   );
