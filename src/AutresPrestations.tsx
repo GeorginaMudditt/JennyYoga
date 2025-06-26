5115e546bf39969dfaaaa4469f4c39ebb1f8b942
Georgina Mudditt <g.k.mudditt@gmail.com>
2025-06-26 12:33:17 +0200

    add sauna and coaching images

--- src/AutresPrestations.tsx
+++ src/AutresPrestations.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "./AutresPrestations.css";
 import meditation from "./assets/meditation-new.jpeg";
 import massage from "./assets/massage-new.jpeg";
+import coaching from "./assets/coaching-new.jpeg";
+import sauna from "./assets/sauna-hammam-new.jpeg";
 
 function AutresPrestations() {
   return (
     <>
       <div className="AutresPrestations">
-        <div className="massage-text">
+        <div className="text-container">
           <h1>Autres Prestations</h1>
 
           <div className="container">
@@ -36,7 +38,7 @@ function AutresPrestations() {
           </div>
           {/* <button className="btn meditation">Plus d'informations</button> */}
           <div className="container">
-            <div className="meditation-description">
+            <div className="massage-description">
               <h2 id="massage-section">Massage et soins énergétiques</h2>
               <p>
                 Le massage ( massage des 5 continents, massage énergétique et
@@ -55,29 +57,43 @@ function AutresPrestations() {
             <p>Sur réservation au Studio, espace Massage</p>
           </div>
           {/* <button className="btn massages">Plus d'informations</button> */}
-          <h2 id="coaching-section">Coaching Holistique</h2>
-          <p>
-            Le coaching holistique accompagne vers la réalisation de soi et la
-            puissance créative. Déprogrammation, développement personnel en
-            intégrant l’esprit, le corps et les émotions. Accompagnement et à
-            l'introspection, pour trouver l’équilibre, surmonter les défis et
-            atteindre ses objectifs, pour favoriser le bien-être,
-            l'épanouissement et la manifestation positif et de la joie
-            intérieure.
-          </p>
+          <div className="container">
+            <img
+              className="coaching-image"
+              src={coaching}
+              alt="holistic coaching"
+            />
+            <div className="coaching-description">
+              <h2 id="coaching-section">Coaching Holistique</h2>
+              <p>
+                Le coaching holistique accompagne vers la réalisation de soi et
+                la puissance créative. Déprogrammation, développement personnel
+                en intégrant l’esprit, le corps et les émotions. Accompagnement
+                et à l'introspection, pour trouver l’équilibre, surmonter les
+                défis et atteindre ses objectifs, pour favoriser le bien-être,
+                l'épanouissement et la manifestation positif et de la joie
+                intérieure.
+              </p>
+            </div>
+          </div>
           <div className="coaching-info">
             <p>Sur rdv et consultation au Studio</p>
           </div>
           {/* <button className="btn coaching">Plus d'informations</button> */}
-          <h2 id="sauna-section">Sauna | Hammam</h2>
-          <p>
-            Le sauna et le hammam sont des thérapies de relaxation par la
-            chaleur qui détoxifient le corps et procurent une détente profonde.
-            Le sauna diffuse une chaleur sèche, tandis que le hammam offre de la
-            vapeur. Ils améliorent la circulation, accélére le métabolisme,
-            soulagent les tensions musculaires et purifient la peau et votre
-            organisme, favorisant le bien-être.
-          </p>
+          <div className="container">
+            <div className="sauna-description">
+              <h2 id="sauna-section">Sauna | Hammam</h2>
+              <p>
+                Le sauna et le hammam sont des thérapies de relaxation par la
+                chaleur qui détoxifient le corps et procurent une détente
+                profonde. Le sauna diffuse une chaleur sèche, tandis que le
+                hammam offre de la vapeur. Ils améliorent la circulation,
+                accélére le métabolisme, soulagent les tensions musculaires et
+                purifient la peau et votre organisme, favorisant le bien-être.
+              </p>
+            </div>
+            <img className="sauna-image" src={sauna} alt="sauna" />
+          </div>
           <div className="sauna-info">
             <p>Sur réservation au Studio, espace Spa</p>
           </div>
